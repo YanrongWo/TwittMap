0a2640ff954ae9e2817532ec36198c5f125f513d
Rona (Yanrong Wo) <yw2513@columbia.edu>
2016-02-24 16:31:42 -0500

    Added google map

--- web_application/server.py
+++ web_application/server.py
@@ -1,4 +1,38 @@
 from flask import Flask, request, render_template, g, redirect, Response, make_response
+import os
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+	return render_template("index.html")
+
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using
+
+        python server.py
+
+    Show the help text using
+
+        python server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    print "running on %s:%d" % (HOST, PORT)
+    app.run(host=HOST, port=PORT, debug=True, threaded=threaded)
+
+
+  run()
