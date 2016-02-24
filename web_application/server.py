ad108df3877d66ce41d573bf181f721a14a49039
Aubrey Alston <aubrey.alston.1@gmail.com>
2016-02-24 16:23:48 -0500

    Added file header to server.py; placed configuration check in server.py.

--- web_application/server.py
+++ web_application/server.py
@@ -1,3 +1,14 @@
+####################################################################
+# web_application/server.py
+# COMS E6998, Assignment 1 - TwittMap
+# 
+# Performs routing for the TwittMap web application.
+#
+# Dependencies: elasticsearch
+####################################################################
+
+from __init__ import * # Ensure proper configuration is in place for querying tweets
+
 from flask import Flask, request, render_template, g, redirect, Response, make_response
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
