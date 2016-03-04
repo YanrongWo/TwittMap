6c4c9358112320558a9aa80ec2365f2a1ffb620e
Rona (Yanrong Wo) <yw2513@columbia.edu>
2016-03-04 15:41:43 -0500

    Restructured project for deployment.

--- web_application/__init__.py
+++ web_application/__init__.py
@@ -13,12 +13,24 @@ import sys
 import json
 
 if os.environ.get("TWITTMAP_WEB_CONFIGURED") == None:
-	with open(os.path.dirname(os.path.realpath(__file__))+"/../config.json", 'r') as f:
+	with open(os.path.dirname(os.path.realpath(__file__))+"config.json", 'r') as f:
 		config = json.loads(f.read())
 
 		os.environ["TWITTMAP_ES_NODES"] = json.dumps(config["elasticsearch"]["nodes"])
 
 		os.environ["TWITTMAP_WEB_CONFIGURED"] = "1"
+
+if os.environ.get("TWITTMAP_CONFIGURED") == None:
+	with open(os.path.dirname(os.path.realpath(__file__))+"config.json", 'r') as f:
+		config = json.loads(f.read())
+
+		os.environ["TWITTMAP_ES_NODES"] = json.dumps(config["elasticsearch"]["nodes"])
+		os.environ["TWITTMAP_TWITTER_CONSUMER_KEY"] = config["twitter"]["consumer_key"]
+		os.environ["TWITTMAP_TWITTER_CONSUMER_SECRET"] = config["twitter"]["consumer_secret"]
+		os.environ["TWITTMAP_TWITTER_ACCESS_TOKEN"] = config["twitter"]["access_token"]
+		os.environ["TWITTMAP_TWITTER_ACCESS_TOKEN_SECRET"] = config["twitter"]["access_token_secret"]
+
+		os.environ["TWITTMAP_CONFIGURED"] = "1"
 
 if os.environ.get("TWITTMAP_WEB_CONFIGURED") == None:
 	print "Invalid or missing configuration."
